e6fb179a890e1fa81b9ccc08111dd4186b39adef
Natthakit <nonusae@gmail.com>
2016-12-18 18:18:31 +0700

    implemented enum for status to post for improval workflow

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
+	enum status: { submmited: 0, approved: 1,rejected: 2 }
 	belongs_to :user
 	validates_presence_of :date, :rationale
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161204091605) do
+ActiveRecord::Schema.define(version: 20161218110257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20161204091605) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.integer  "status", default: 0
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
